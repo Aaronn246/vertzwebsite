5dacc41e1be78b59543dffb6105acea7126034ec
Supreme AA <121055281+Aaronn246@users.noreply.github.com>
2023-02-23 16:13:22 -0400

    Create home.js

---
+++ js/home.js
@@ -0,0 +1,15 @@
+var menu_ui = document.getElementById('menu-ui');
+
+var menu_btn_clicked = false;
+
+function menu_btn_click(){
+    if (menu_btn_clicked == false){
+        menu_ui.style.display = 'block';
+        setTimeout(() => { menu_ui.style.transform = 'scale(1)'; }, 1);
+        menu_btn_clicked = true;
+    } else{
+        setTimeout(() => { menu_ui.style.transform = 'scale(0.9)'; }, 1);
+        setTimeout(() => { menu_ui.style.display = 'none';; }, 100);
+        menu_btn_clicked = false;
+    }
+};
