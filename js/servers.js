d8a5d7ca1f018fc41942d10f6f0fd79167e10e64
Supreme AA <121055281+Aaronn246@users.noreply.github.com>
2023-02-24 09:12:18 -0400

    Update servers.js

--- js/servers.js
+++ js/servers.js
@@ -19,3 +19,23 @@ function close_menu(){
     menu_ui.style.transform = 'scale(0.9)';
     menu_btn_clicked = false;
 };
+
+var search_txt = document.getElementById('search-txt');
+
+function search(element){
+    var input, filter, servers, div, h1, i, txtValue;
+    input = search_txt;
+    filter = input.value.toUpperCase();
+    servers = document.getElementById('servers');
+    div = servers.getElementsByTagName('div');
+
+    for (i = 0; i < div.length; i++) {
+        h1 = div[i].getElementsByTagName('h1')[0];
+        txtValue = h1.textContent || h1.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            div[i].style.display = 'block';
+        } else {
+            div[i].style.display = 'none';
+        }
+    }
+};
